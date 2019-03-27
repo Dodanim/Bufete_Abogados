ab157f60f3887d1bd011db534c1fae3344443114
KiDo <KiDo@KiDo-PC>
2019-03-27 16:46:45 -0600

    a ver donde diablos vas

--- src/java/com/convert/BitacoraConvert.java
+++ src/java/com/convert/BitacoraConvert.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+prueba para github
  */
 package com.convert;
 
