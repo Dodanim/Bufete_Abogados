d12258040ebe9ecd03441c9a01051063184168af
KiDo <KiDo@KiDo-PC>
2019-03-27 18:57:36 -0600

    a ver, acabo de cambiar un comentario para ver si sube cambios

--- src/java/com/convert/BitacoraConvert.java
+++ src/java/com/convert/BitacoraConvert.java
@@ -2,7 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
-prueba para si cambio algo aqui, tiene que salir?
+prueba para si cambio algo aqui, tiene que salir? hhhh
  */
 package com.convert;
 
